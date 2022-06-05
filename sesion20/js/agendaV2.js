05418fc21da10e089452a697ce3c701b88253fa5
dsuclupe <dianaaracelysuclupegalvez@gmail.com>
2022-06-04 23:18:18 -0500

    respaldo

--- sesion20/js/agendaV2.js
+++ sesion20/js/agendaV2.js
@@ -26,13 +26,14 @@ function eliminarTodo(){
 
 function actualizarDatos() {
     var registro="";
-    if (localStorage.length===0) {
+    if (localStorage.length === 0) {
         registro += '<li>Vacio</li>';
     } else {
         for (var i = 0; i <=localStorage.length -1 ; i++) {
             var key= localStorage.key(i);
             registro += '<li>' + '<span class="nom">' + key + '</span>'
-            +'<span class="movil">' +localStorage.getItem(key)+'</span>'+'</li><br>';
+            +'<span class="nom">' 
+            +localStorage.getItem(key)+'</span>'+'</li><br>';
         }
     }
     document.getElementById('contactos').innerHTML=registro;
--- sesion20/js/agendaV3.js
+++ sesion20/js/agendaV3.js
@@ -1,51 +1,102 @@
-function guardarDatos(){
-    //capturando variables
-    const nombre=document.getElementById("nombre").value;
-    const movil=document.getElementById("movil").value;
-    const email=document.getElementById("email").value;
+// function guardarDatos(){
+//     //capturando variables
+//     const nombre=document.getElementById("nombre").value;
+//     const movil=document.getElementById("movil").value;
+//     const email=document.getElementById("email").value;
 
-    const datos={
-        'movil':movil,
+//     const datos={
+//         'movil':movil,
+//         'email':email,
+//     };
+//     //almacena datos
+//     localStorage.setItem(nombre, JSON.stringify(datos));
+//     //borrando los datos
+//     document.getElementById("nombre").value="";
+//     document.getElementById("movil").value="";
+//     document.getElementById("email").value="";
+//     //actualizando la lista
+//     actualizarDatos();
+// }
+
+// function recuperarDatos() {
+//     var nombre= document.getElementById("nombre").value=localStorage.getItem(nombre)
+//     document.getElementById("movil").value=localStorage.getItem(nombre);
+//     document.getElementById("email").value=localStorage.getItem(nombre);
+
+// }
+
+// function eliminarDato() {
+//     var nombre=document.getElementById("nombre").value;
+//     localStorage.removeItem(nombre);
+//     actualizarDatos();
+// }
+
+// function eliminarTodo(){
+//     localStorage.clear();
+//     actualizarDatos();
+// }
+
+// function actualizarDatos() {
+//     var registro="";
+//     if (localStorage.length===0) {
+//         registro += '<li>Vacio</li>';
+//     } else {
+//         for (var i = 0; i <=localStorage.length -1 ; i++) {
+//             var key= localStorage.key(i);
+//             registro += '<li>' + '<span class="nom">' + key + '</span>'
+//             +'<span class="movil">' +localStorage.getItem(key)+'</span>'+'<span class="email">' +localStorage.getItem(key)+'</li><br>';
+//         }
+//     }
+//     document.getElementById('contactos').innerHTML=registro;
+// }
+
+
+function guardarDatos() {
+    var nombre = document.getElementById("nombre").value;
+    var movil = document.getElementById("movil").value;
+    var email  = document.getElementById("email").value;
+    //-----------------------//
+    var datos={
+        'movil': movil,
         'email':email,
+        
     };
-    //almacena datos
+    //-----------------------//
     localStorage.setItem(nombre, JSON.stringify(datos));
-    //borrando los datos
-    document.getElementById("nombre").value="";
-    document.getElementById("movil").value="";
-    document.getElementById("email").value="";
-    //actualizando la lista
+    document.getElementById("nombre").value = "";
+    document.getElementById("movil").value = "";
+    
     actualizarDatos();
 }
-
 function recuperarDatos() {
-    var nombre= document.getElementById("nombre").value=localStorage.getItem(nombre)
-    document.getElementById("movil").value=localStorage.getItem(nombre);
-    document.getElementById("email").value=localStorage.getItem(nombre);
-
+    var nombre = document.getElementById("nombre").value;
+    localStorage.getItem(nombre);
+    document.getElementById("movil").value = localStorage.getItem(nombre);
 }
-
-function eliminarDato() {
-    var nombre=document.getElementById("nombre").value;
+function eliminarDatos() {
+    var nombre = document.getElementById("nombre").value;
     localStorage.removeItem(nombre);
     actualizarDatos();
 }
-
-function eliminarTodo(){
+function eliminarTodo() {
     localStorage.clear();
     actualizarDatos();
 }
-
 function actualizarDatos() {
-    var registro="";
-    if (localStorage.length===0) {
+    var registro = "";
+    if (localStorage.length === 0) {
         registro += '<li>Vacio</li>';
     } else {
-        for (var i = 0; i <=localStorage.length -1 ; i++) {
-            var key= localStorage.key(i);
-            registro += '<li>' + '<span class="nom">' + key + '</span>'
-            +'<span class="movil">' +localStorage.getItem(key)+'</span>'+'<span class="email">' +localStorage.getItem(key)+'</li><br>';
+        for (var index = 0; index < localStorage.length; index++) {
+            var key = localStorage.key(index);
+            var datsoss=JSON.parse(localStorage.getItem(key));
+            registro += '<li>' + '<span class="nom">' + key + '</span>' 
+            + '<span class ="nom">' + datsoss.email+ '</span>' 
+            + '<span class ="nom">' + datsoss.movil+ '</span>' 
+            + '</li><br>';
         }
     }
-    document.getElementById('contactos').innerHTML=registro;
+    document.getElementById('contactos').innerHTML = registro;
 }
+
+
